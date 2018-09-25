5ae3e16179056b1d03a77a45e03c47e880530c3e
Rodrigo Presa <rodrigopresamar@gmail.com>
2018-09-24 21:55:46 -0600

    Creacion del proyecto

---
+++ src/main/java/com/bib404/system_bib404/SystemBib404Application.java
@@ -0,0 +1,12 @@
+package com.bib404.system_bib404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBib404Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemBib404Application.class, args);
+	}
+}
---
+++ src/test/java/com/bib404/system_bib404/SystemBib404ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bib404.system_bib404;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SystemBib404ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
