a5539049b11aa05f0c2962d378ef69a5a135e4ae
dixon28 <33327336+dixon28@users.noreply.github.com>
2018-11-26 02:40:55 -0600

    Eliminar
    esta programado el boton eliminar pero por  la integridad referencial no permite eliminar los objetos

--- src/main/java/com/bib404/system_bib404/Repository/BibliotecaRepository.java
+++ src/main/java/com/bib404/system_bib404/Repository/BibliotecaRepository.java
@@ -6,9 +6,18 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import com.bib404.system_bib404.entity.Biblioteca;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.bib404.system_bib404.entity.Biblioteca;
+import com.bib404.system_bib404.entity.TipoRecurso;
+@Transactional(readOnly=true)
 @Repository("bibliotecaRepository")
 public interface BibliotecaRepository extends JpaRepository<Biblioteca, Serializable>{
   
+
+	
+	@Query(value = "select * from BIBLIOTECA where BIBLIOTECA.ID = ? ", 
+	  nativeQuery = true)
+public Biblioteca buscarBiblioteca(int ID);
+
 }
--- src/main/java/com/bib404/system_bib404/controller/gestionar_recursos_bib/GestionarBIBController.java
+++ src/main/java/com/bib404/system_bib404/controller/gestionar_recursos_bib/GestionarBIBController.java
@@ -17,14 +17,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bib404.system_bib404.Repository.BibliotecaRepository;
 import com.bib404.system_bib404.Repository.PerfilRepository;
 import com.bib404.system_bib404.Repository.RecursoBibliotecarioRepository;
 import com.bib404.system_bib404.Repository.TipoRecursoRepository;
 import com.bib404.system_bib404.constant.Template;
+import com.bib404.system_bib404.entity.Biblioteca;
 import com.bib404.system_bib404.entity.Municipio;
 import com.bib404.system_bib404.entity.RecursoBibliotecario;
 import com.bib404.system_bib404.entity.TipoRecurso;
 import com.bib404.system_bib404.entity.Usuario;
+import com.bib404.system_bib404.service.impl.UsuarioServiceImpl;
 
 @Controller
 @RequestMapping("/")
@@ -39,7 +42,13 @@ public class GestionarBIBController {
 	@Qualifier("recursoBibliotecarioRepository")
 	private RecursoBibliotecarioRepository rbr;
 	
+	@Autowired
+	@Qualifier("usuarioServiceImpl")
+	private UsuarioServiceImpl usuarioImp;
 	
+	@Autowired
+	@Qualifier("bibliotecaRepository")
+	private BibliotecaRepository br;
 	
 	
 	@GetMapping("/recursos")
@@ -54,6 +63,7 @@ public class GestionarBIBController {
 			    
 		model.addAttribute("TipoRecurso", tr);
         model.addAttribute("recursoBib", lrb);
+        model.addAttribute("bibliotecas", usuarioImp.listBibliotecas());
 
 		return Template.GESTION;
 		
@@ -61,16 +71,29 @@ public class GestionarBIBController {
 	
 	
 	@PostMapping("/addRecurso")
-	public String addContra(@Valid@ModelAttribute("RecursoBibliotecario") RecursoBibliotecario recurso,@ModelAttribute("tipoR") String tipR, Model model,HttpServletRequest request) {
+	public String addContra(@Valid@ModelAttribute("RecursoBibliotecario") RecursoBibliotecario recurso,@ModelAttribute("tipoR") String tipR,@ModelAttribute("bib") String bib, Model model,HttpServletRequest request) {
 		HttpSession sesion = request.getSession();
 		TipoRecurso tiporecurso;
+		Biblioteca biblioteca;
 		int id_tipo = Integer.parseInt(tipR);
+		int id_biblioteca = Integer.parseInt(bib);
 		tiporecurso=trr.buscarRecurso(id_tipo);
-		
+		biblioteca=br.buscarBiblioteca(id_biblioteca);
 		recurso.setTipo_recurso(tiporecurso);
+		recurso.setBiblioteca(biblioteca);
 	
 		rbr.save(recurso);
 
 		return "redirect:/recursos";
 }
+	
+@PostMapping("/removeRecurso")
+
+public String removerRecurso(@RequestParam("dato") String dato) {
+	int id_dato = Integer.parseInt(dato);
+	rbr.deleteById(id_dato);
+	   
+	return "redirect:/recursos";
+}
+	
 }
